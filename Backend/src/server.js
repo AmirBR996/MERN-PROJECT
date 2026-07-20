2f9492cd4fd5c8dd215476cd716b892dc695c7ec
Amir <amirgulmeli123@gmail.com>
2026-07-20 13:25:18 +0545

    krishik

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,11 +7,7 @@ import cors from "cors";
 import userRoutes from "./routes/user_routes.js";
 import productRoutes from "./routes/product_routes.js";
 import orderRoutes from "./routes/order_routes.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -29,12 +25,6 @@ app.use("/users", userRoutes);
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
-const frontendPath = path.join(__dirname, "../../Front_end/krishik/dist");
-app.use(express.static(frontendPath));
-
-app.get("/{*splat}", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
-});
 
 const server = http.createServer(app);
 
