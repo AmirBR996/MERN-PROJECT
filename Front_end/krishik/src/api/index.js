449616f7b5221baf7c4222d8f0f8824f0b86d2e9
Amir <amirgulmeli123@gmail.com>
2026-07-20 13:58:08 +0545

    krishik

--- Front_end/krishik/src/api/index.js
+++ Front_end/krishik/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.PROD ? "" : "http://localhost:8080",
+  baseURL: import.meta.env.PROD ? "" : "https://krishik-bazar-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
